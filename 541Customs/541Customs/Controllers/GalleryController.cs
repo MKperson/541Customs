011c95a3f1c8cec7cdfb602c911851f7b4c8d270
Mike Kvederis <m_kvederis@msn.com>
2018-02-15 12:25:28 -0800

    Adding all the things

---
+++ 541Customs/541Customs/Controllers/GalleryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using _541Customs.Models;
+
+namespace _541Customs.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 541Customs/541Customs/Models/PictureClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _541Customs.Models
+{
+    public class PictureClass
+    {
+        
+        
+        public List<string> FileNames { get; set; }
+
+
+        public PictureClass()
+        {
+            DirectoryInfo files = new DirectoryInfo(@".\wwwroot\images\");
+            FileInfo[] fileinfos = null;
+            try
+            {
+                fileinfos = files.GetFiles();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            FileNames = files.GetFiles().Select(x => x.Name).ToList();
+            
+            
+        }
+    }
+}
